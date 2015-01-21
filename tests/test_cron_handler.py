445d0ec1629366fa816d46f93225afba180ca8d8
heavenshell <heavenshell.jp@gmail.com>
2015-01-21 20:59:09 +0900

    Add tests.

--- tests/test_cron_handler.py
+++ tests/test_cron_handler.py
@@ -18,6 +18,9 @@ class TestScheduler(TestCase):
     def setUpClass(cls):
         cls.scheduler = Scheduler()
 
+    def tearDown(self):
+        self.scheduler.scheduler.remove_all_jobs()
+
     def test_should_parse_cron_expression(self):
         """ Scheduler().parse_cron_expression() should parse to `apscheduler` expressions. """
         ret = self.scheduler.parse_cron_expression('15 1 5 12 6')
@@ -43,8 +46,37 @@ class TestScheduler(TestCase):
     def test_should_list_jobs(self):
         self.scheduler.add_job('15 1 1 12 *', 'test job')
         jobs = self.scheduler.list_job()
-        self.assertEqual(jobs[0]['message'], 'test job')
-        self.assertEqual(jobs[0]['trigger'], '15 1 1 12 *')
+        self.assertTrue('"15 1 1 12 *"' in jobs[0])
+        self.assertTrue('test job' in jobs[0])
+
+    def test_should_delete_job(self):
+        """ Scheduler().remove_job() shoudl return True when job remove sucess. """
+        job = self.scheduler.add_job('15 1 1 12 *', 'test job')
+        ret = self.scheduler.remove_job(job.id)
+        self.assertTrue(ret)
+
+    def test_should_delete_job_fail(self):
+        """ Scheduler().remove_job() should return True when job remove sucess. """
+        ret = self.scheduler.remove_job('foobar')
+        self.assertFalse(ret)
+
+    def test_should_pouse_job(self):
+        """ Scheduler().pause_job() should pause registered job. """
+        job = self.scheduler.add_job('15 1 1 12 *', 'test job')
+        self.scheduler.pause_job(job.id)
+        jobs = self.scheduler.list_job()
+        self.assertTrue('paused' in jobs[0])
+
+    def test_should_resume_job(self):
+        """ Scheduler().resume_job() should resume paused job. """
+        job = self.scheduler.add_job('15 1 1 12 *', 'test job2')
+        self.scheduler.pause_job(job.id)
+        jobs = self.scheduler.list_job()
+        self.assertTrue('paused' in jobs[0])
+
+        self.scheduler.resume_job(job.id)
+        jobs = self.scheduler.list_job()
+        self.assertFalse('paused' in jobs[0])
 
 
 class TestCronHandler(TestCase):
